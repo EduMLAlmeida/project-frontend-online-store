7203999c65360d42139d2251835bf01945832c1c
Gabriel Pesch <gkpesch@gmail.com>
2022-03-11 20:51:58 -0300

    Add: feat ProductCategory input search

--- src/components/Products.js
+++ src/components/Products.js
@@ -4,27 +4,31 @@ import PropTypes from 'prop-types';
 class Products extends Component {
   render() {
     const { searchResult } = this.props;
+    console.log(searchResult);
     return (
       searchResult === []
         ? (<p>Nenhum produto foi encontrado</p>)
         : (
           <div>
             {
-              (searchResult.map((currProduct) => {
-                const {
-                  id,
-                  title,
-                  price,
-                  thumbnail,
-                } = currProduct;
-                return (
-                  <div key={ id }>
-                    <p>{title}</p>
-                    <img src={ thumbnail } alt={ title } />
-                    <p>{price}</p>
-                  </div>
-                );
-              }))
+              (searchResult
+                .filter((_, index) => index > 0)
+                .map((currProduct) => {
+                  const {
+                    id,
+                    title,
+                    price,
+                    thumbnail,
+                  } = currProduct;
+                  return (
+                    <div key={ id }>
+                      <span data-testid="product" />
+                      <p>{title}</p>
+                      <img src={ thumbnail } alt={ title } />
+                      <p>{price}</p>
+                    </div>
+                  );
+                }))
             }
           </div>
         )
