ec4341d870ae4cb94578e69d9751ad6b121781f5
IanSilveira <iansilveira2014@hotmail.com>
2022-03-11 15:26:57 -0300

    Requisito 4 feito!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import ProductCategoryList from '../components/ProductCategoryList';
 
 export default class Home extends Component {
   render() {
     return (
-      <h2 data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </h2>
+      <>
+        <h2 data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </h2>
+        <ProductCategoryList />
+      </>
     );
   }
 }
